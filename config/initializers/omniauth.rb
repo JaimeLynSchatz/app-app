a709986229c81c301add107db49cbb2c714bd56c
Bookis <vegan.bookis@gmail.com>
2013-08-06 17:08:21 -0700

    update links

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,  "de9b2fbd43c7b20fbf88",  "0e5efd7b78ec5df3e14c2fa5350796077d062403"
+  provider :github,  ENV["GITHUB_KEY"],  ENV["GITHUB_SECRET"]
 end
